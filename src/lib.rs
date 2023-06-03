79da0a86e4ea50c4da9d40b79787627ba19024a8
Andrej Balaz <andrejbalaz001@gmail.com>
2023-06-03 18:00:09 +0200

    added generic wrapper around suffix array

--- src/lib.rs
+++ src/lib.rs
@@ -114,6 +114,57 @@ where
     return map;
 }
 
+pub mod arrays {
+    // https://stackoverflow.com/a/41597323
+    use bio::data_structures::suffix_array::suffix_array;
+
+    pub trait SuffixArray {
+        fn create(&self) -> Vec<usize>;
+    }
+
+    impl SuffixArray for [u8] {
+        fn create(&self) -> Vec<usize> {
+            return suffix_array(self);
+        }
+    }
+
+    impl SuffixArray for [u64] {
+        fn create(&self) -> Vec<usize> {
+            let n = self.len();
+            let mut v: Vec<(&[u64], usize)> = Vec::with_capacity(n);
+            for i in 0..n {
+                v.push((&self[i..], i));
+            }
+            v.sort_unstable();
+            let result = v.iter().map(|x| x.1).collect();
+            return result;
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use crate::arrays::SuffixArray;
+
+        #[test]
+        fn test_suffix_array() {
+            let v: Vec<u8> = b"GATCAG$".to_vec();
+            let sa = SuffixArray::create(&v[..]);
+            // 6: $GATCAG
+            // 4: AG$GATC
+            // 1: ATCAG$G
+            // 3: CAG$GAT
+            // 5: G$GATCA
+            // 0: GATCAG$
+            // 2: TCAG$GA
+            assert_eq!(vec![6, 4, 1, 3, 5, 0, 2], sa);
+
+            let v: Vec<u64> = vec![3, 1, 4, 2, 1, 3, 0];
+            let sa = SuffixArray::create(&v[..]);
+            assert_eq!(vec![6, 4, 1, 3, 5, 0, 2], sa);
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
