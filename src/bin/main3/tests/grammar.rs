a811a69cb3774619cd9dd791e904f8263fff9064
Andrej Balaz <andrejbalaz001@gmail.com>
2023-06-14 16:06:47 +0200

    added proptest

--- src/bin/main3/tests/grammar.rs
+++ src/bin/main3/tests/grammar.rs
@@ -2,6 +2,7 @@ use crate::grammar::Grammar;
 use std::{process::Command, fs::File};
 use std::str;
 use std::io::Write;
+use proptest::prelude::*;
 
 #[test]
 fn test() {
@@ -41,5 +42,38 @@ fn complex_test() {
     println!("Testing characters...");
     for i in 0..g.len() {
         assert_eq!(g[i], s[i]);
+    }
+}
+
+proptest! {
+    #[test]
+    fn proptesting(s in "[ACGT]*") {
+        let s: Vec<_> = s.bytes().collect();
+        {
+            let mut out = File::create("data/temporary/seq.txt").expect("Cannot create file.");
+            write!(out, "{}", str::from_utf8(&s).unwrap()).expect("Cannot write to file.");
+        }
+
+        println!("Building grammar...");
+        Command::new("./tools/bigrepair/bigrepair")
+            .args(["data/temporary/seq.txt"])
+            .output()
+            .expect("Failer to run bigrepair");
+
+        println!("Converting rules to plaintext...");
+        Command::new("./scripts/print_plain_slp")
+            .args(["data/temporary/seq.txt"])
+            .output()
+            .expect("Failed to convert to plain text");
+
+        println!("Loading grammar...");
+        let g = Grammar::from_file("data/temporary/seq.txt.plainslp");
+
+        println!("Testing lengths...");
+        assert_eq!(g.len(), s.len());
+        println!("Testing characters...");
+        for i in 0..g.len() {
+            assert_eq!(g[i], s[i]);
+        }
     }
 }
