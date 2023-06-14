e5ed68edaa7f8d623a50ffc05c7fc8d93f404ba3
Andrej Balaz <andrejbalaz001@gmail.com>
2023-06-14 23:00:03 +0200

    used proptest to find bad grammar

--- src/bin/main3/tests/grammar.rs
+++ src/bin/main3/tests/grammar.rs
@@ -14,9 +14,7 @@ fn test() {
     println!();
 }
 
-#[test]
-fn complex_test() {
-    let s = b"AGCTTGCGTAGCTAGCTGAGCTGATCG".to_owned();
+fn test_grammar_for_string(s: &[u8]) {
     {
         let mut out = File::create("data/temporary/seq.txt").expect("Cannot create file.");
         write!(out, "{}", str::from_utf8(&s).unwrap()).expect("Cannot write to file.");
@@ -45,35 +43,17 @@ fn complex_test() {
     }
 }
 
+#[test]
+fn complex_test() {
+    // let s = b"AGCTTGCGTAGCTAGCTGAGCTGATCG".to_owned();
+    let s = b"TGACGGGCAGT".to_owned();
+    test_grammar_for_string(&s);
+}
+
 proptest! {
     #[test]
-    fn proptesting(s in "[ACGT]*") {
+    fn proptesting(s in "[ACGT]+") {
         let s: Vec<_> = s.bytes().collect();
-        {
-            let mut out = File::create("data/temporary/seq.txt").expect("Cannot create file.");
-            write!(out, "{}", str::from_utf8(&s).unwrap()).expect("Cannot write to file.");
-        }
-
-        println!("Building grammar...");
-        Command::new("./tools/bigrepair/bigrepair")
-            .args(["data/temporary/seq.txt"])
-            .output()
-            .expect("Failer to run bigrepair");
-
-        println!("Converting rules to plaintext...");
-        Command::new("./scripts/print_plain_slp")
-            .args(["data/temporary/seq.txt"])
-            .output()
-            .expect("Failed to convert to plain text");
-
-        println!("Loading grammar...");
-        let g = Grammar::from_file("data/temporary/seq.txt.plainslp");
-
-        println!("Testing lengths...");
-        assert_eq!(g.len(), s.len());
-        println!("Testing characters...");
-        for i in 0..g.len() {
-            assert_eq!(g[i], s[i]);
-        }
+        test_grammar_for_string(&s);
     }
 }
